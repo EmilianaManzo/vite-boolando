111388716f9ea83ca20ec130bc0d1f8c6f6b5cdd
EmilianaManzo <emiliana.ma96@gmail.com>
2024-03-21 15:46:05 +0100

    [add] components and sass file

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,29 @@
 
 <script>
+import Header from './components/Header.vue';
+import Main from './components/Main.vue'
+import Footer from './components/Footer.vue'
   export default {
-    
-  }
+    components:{
+      Header,
+      Main,
+      Footer
+    },
+
+  };
 </script>
 
 
 <template>
   <div>
+    
+    <Header />
+    <Main />
+    <Footer />
 
   </div>
 </template>
 
-<style lang="scss">
+<style lang="scss" >
   @use './assets/scss/main.scss'
 </style>
